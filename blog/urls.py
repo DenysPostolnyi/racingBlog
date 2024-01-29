09212d7cc380cb0855a6b4a0e7481bf29db4eb17
DenysPostolnyi <postlden44@gmail.com>
2024-01-29 10:00:19 +0200

    changed views format

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.index, name='main-page'),
-    path('posts', views.posts, name='posts-page'),
-    path('posts/<str:slug>', views.post_detail, name='post-detail-page'),
+    path('', views.StartingPageView.as_view(), name='main-page'),
+    path('posts', views.AllPostsView.as_view(), name='posts-page'),
+    path('posts/<str:slug>', views.SinglePostView.as_view(), name='post-detail-page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,67 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
 
 # Create your views here.
 
-def index(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    # latest_posts = sorted(posts, key=lambda item: item["date"], reverse=True)[-3:]
-    return render(request, 'index.html', {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    # index
+    template_name = 'index.html'
+    model = Post
+    ordering = ["-date"]
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'all-posts.html', {
-        "all_posts": all_posts
-    })
+# def index(request):
+#     latest_posts = Post.objects.all().order_by('-date')[:3]
+#     # latest_posts = sorted(posts, key=lambda item: item["date"], reverse=True)[-3:]
+#     return render(request, 'index.html', {
+#         "posts": latest_posts
+#     })
+
+class AllPostsView(ListView):
+    # posts
+    template_name = 'all-posts.html'
+    model = Post
+    ordering = ["-date"]
+    context_object_name = 'all_posts'
 
 
-def post_detail(request, slug):
-    post_details = get_object_or_404(Post, slug=slug)
-    return render(request, 'post-details.html', {
-        "post": post_details,
-        "tags": post_details.tags.all()
-    })
-    # try:
-    #     founded_post = next(post for post in posts_data if post['slug'] == slug)
-    #     return render(request, 'post-details.html', {
-    #         "post": founded_post
-    #     })
-    # except StopIteration:
-    #     return render(request, '404.html')
+# def posts(request):
+#     all_posts = Post.objects.all().order_by('-date')
+#     return render(request, 'all-posts.html', {
+#         "all_posts": all_posts
+#     })
+
+
+class SinglePostView(DetailView):
+    # post_detail
+    template_name = 'post-details.html'
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = self.object.tags.all()
+        return context
+
+# def post_detail(request, slug):
+#     post_details = get_object_or_404(Post, slug=slug)
+#     return render(request, 'post-details.html', {
+#         "post": post_details,
+#         "tags": post_details.tags.all()
+#     })
+
+# try:
+#     founded_post = next(post for post in posts_data if post['slug'] == slug)
+#     return render(request, 'post-details.html', {
+#         "post": founded_post
+#     })
+# except StopIteration:
+#     return render(request, '404.html')
