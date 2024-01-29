c4d37df710fc2de5271fd54c76f8fbcfdc7cd967
DenysPostolnyi <postlden44@gmail.com>
2024-01-29 10:00:38 +0200

    added Comments model

--- blog/models.py
+++ blog/models.py
@@ -34,3 +34,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + " " + self.date.__str__()
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    email = models.EmailField()
+    text = models.TextField(max_length=500)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
